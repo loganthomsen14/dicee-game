67d8def4b5faf5882ecf1387eb47276312ad8a67
Logan Thomsen <loganthomsen14@gmail.com>
2022-03-30 17:04:05 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// Random Numbers
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+console.log(randomNumber1) // This makes the first random number
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber2) // This makes the second random number
+
+// if Statements
+
+if (randomNumber1 === 6) {
+    document.getElementById("img1").src = "images/dice6.png"    
+} else if (randomNumber1 === 5) {
+    document.getElementById("img1").src = "images/dice5.png"
+} else if (randomNumber1 === 4) {
+    document.getElementById("img1").src = "images/dice4.png"
+} else if (randomNumber1 === 3) {
+    document.getElementById("img1").src = "images/dice3.png"
+} else if (randomNumber1=== 2) {
+    document.getElementById("img1").src = "images/dice2.png"
+} else if (randomNumber1 === 1) {
+    document.getElementById("img1").src = "images/dice1.png"
+}
+
+
+if (randomNumber2 === 6) {
+    document.getElementById("img2").src = "images/dice6.png"    
+} else if (randomNumber2 === 5) {
+    document.getElementById("img2").src = "images/dice5.png"
+} else if (randomNumber2 === 4) {
+    document.getElementById("img2").src = "images/dice4.png"
+} else if (randomNumber2 === 3) {
+    document.getElementById("img2").src = "images/dice3.png"
+} else if (randomNumber2=== 2) {
+    document.getElementById("img2").src = "images/dice2.png"
+} else if (randomNumber2 === 1) {
+    document.getElementById("img2").src = "images/dice1.png"
+}
+
+
+// Winner Winner
+
+if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw"
+} else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!"
+} else {
+    document.querySelector("h1").innerHTML = "Player 2 wins!"
+}
